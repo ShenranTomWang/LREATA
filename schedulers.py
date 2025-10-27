50a17e64832a6e7a864808f3c149ed8ae8e1624e
ShenranTomWang <wsr.20021128@gmail.com>
2025-10-27 12:30:11 -0700

    LREATA files

--- schedulers.py
+++ schedulers.py
@@ -1,14 +1,14 @@
 import torch
 
 class GaussianAnnealingLR(torch.optim.lr_scheduler._LRScheduler):
-    def __init__(self, optimizer, miu, std, last_epoch=-1):
+    def __init__(self, optimizer, miu: int = 0, sigma: int = 1):
         self.miu = miu
-        self.std = std
-        super().__init__(optimizer, last_epoch)
+        self.sigma = sigma
+        super().__init__(optimizer, 0)
 
     def get_lr(self):
         step = self.last_epoch
-        factor = torch.exp(-0.5 * ((step - self.miu) / self.std) ** 2)
+        factor = torch.exp(-0.5 * ((step - self.miu) / self.sigma) ** 2)
         return [base_lr * factor.item() for base_lr in self.base_lrs]
 
 def get_scheduler(name: str, optimizer: torch.optim.Optimizer, **kwargs) -> torch.optim.lr_scheduler._LRScheduler:
